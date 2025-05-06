d7c489d3a3b7d66cd98d5159cb2ce53878f3af54
Jasper Vos <j.a.c.vos@student.tudelft.nl>
2025-05-06 08:27:14 +0200

    setup csproj, setup versionuntil

---
+++ lib-version/VersionUtil.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace lib_version
+{
+    public class VersionUtil
+    {
+        public Version? CurrentVersion { get; } = typeof(VersionUtil).Assembly.GetName().Version;
+    }
+}
